9d363ee204f157e5b72761239303cf639b533f72
Daniel Herr <daniel.herr@riken.jp>
2018-11-08 17:42:30 +0900

    permutation engine first examples

--- projectq/setups/decompositions/cnot2rotations.py
+++ projectq/setups/decompositions/cnot2rotations.py
@@ -2,23 +2,21 @@
 Registers a decomposition for a CNOT gate into a single multi qubit rotation
 which is implemented using the TimeEvolution gate and additional single qubit rotations.
 """
-
+import cmath
+from projectq.types import Qureg
 from projectq.cengines import DecompositionRule
 from projectq.meta import Compute, get_control_count, Uncompute
-from projectq.ops import X, QubitOperator, TimeEvolution
+from projectq.ops import Rx, Rz, X, QubitOperator, TimeEvolution
 	
 
 def _decompose_cnot_rotation(cmd):
     """ Decompose CNOT gates. """
-    ctrl = cmd.control_qubits
-    target = cmd.qubits
-    eng = cmd.engine
-    qreg = [ctrl, target]
+    qureg = Qureg(cmd.control_qubits + cmd.qubits[0])
     H = QubitOperator("Z0 X1")
     T = TimeEvolution(-cmath.pi/4, H)
-    T | qreg
-    Rz(cmath.pi) | ctrl
-    Rx(cmath.pi) | target
+    T | qureg
+    Rz(cmath.pi) | qureg[0]
+    Rx(cmath.pi) | qureg[1]
 
 
 
--- projectq/setups/surface_codes/lattice_surgery.py
+++ projectq/setups/surface_codes/lattice_surgery.py
@@ -1,8 +1,26 @@
 from projectq.setups import restrictedgateset
 from projectq.setups.decompositions import cnot2rotations
+from projectq.ops import (BasicMathGate, ClassicalInstructionGate, CNOT, BasicRotationGate,
+                          HGate, XGate, YGate, ZGate, TGate, SGate, TimeEvolution, QubitOperator)
+from projectq.cengines import (AutoReplacer, DecompositionRuleSet,
+                               InstructionFilter, LocalOptimizer,
+                               TagRemover, PermutePi4Front)
 
-def _cnot_instruction_filter():
 
+# Write a function which, given a Command object, returns whether the command is supported:
+def is_supported(eng, cmd):
+	if(isinstance(cmd.gate, ClassicalInstructionGate)):
+		return True
+	elif(len(cmd.control_qubits) != 0):
+		return False
+	elif(isinstance(cmd.gate, BasicRotationGate)):
+		return True
+	elif(isinstance(cmd.gate, XGate) or isinstance(cmd.gate, YGate) or isinstance(cmd.gate, ZGate)):
+		return True
+	elif(isinstance(cmd.gate, TGate) or isinstance(cmd.gate, SGate) or isinstance(cmd.gate, HGate)):
+		return True
+	elif(isinstance(cmd.gate, TimeEvolution) or isinstance(cmd.gate, QubitOperator)):
+		return True
 	return False
 
 
@@ -15,9 +33,11 @@ def get_engine_list():
 	rule_set = DecompositionRuleSet(modules=[cnot2rotations]) # this ruleset only impelments a single decompostion
 
 	engines = engines + [AutoReplacer(rule_set),
-							TagRemover(5),
-							InstructionFilter(_cnot_instruction_filter),
+							TagRemover(),
+							InstructionFilter(is_supported),
 							PermutePi4Front()]
+
+#	engines = [AutoReplacer(rule_set),InstructionFilter(is_supported)]
 
 	return engines
 
